91601a9c0991a5d03a776dd0907aa95057db44f6
soyeltigre24 <Jairo.Ariza001@mymdc.net>
2021-04-09 19:45:40 +0000

    home

---
+++ src/js/views/Jumbo.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Jumbo = () => {
+	return (
+		<div className="jumbotron" style={{ background: "grey" }}>
+			<h1 className="display-4">Welcome to SpeedyRX</h1>
+			<p className="lead">
+				Making the Extra for your Comfort <div style={{ color: "blue" }} />
+			</p>
+
+			<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+			<p className="lead">
+				<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwebneel.com%2Fpharmacy-logo&psig=AOvVaw3K6vgIubfnmoHYz4HsKsyx&ust=1618080646349000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJjSiKPq8e8CFQAAAAAdAAAAABAV" />
+			</p>
+		</div>
+	);
+};
+export default Jumbo;
