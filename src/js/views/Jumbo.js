bc447ed49599dd0edf0d615af36fef6d91bcac03
soyeltigre24 <Jairo.Ariza001@mymdc.net>
2021-04-09 22:42:07 +0000

    home page

--- src/js/views/Jumbo.js
+++ src/js/views/Jumbo.js
@@ -5,11 +5,12 @@ const Jumbo = () => {
 		<div className="jumbotron" style={{ background: "grey" }}>
 			<h1 className="display-4">Welcome to SpeedyRX</h1>
 			<p className="lead">
-				Making the Extra for your Comfort <div style={{ color: "blue" }} />
+				Making the extra effort for your Comfort <div style={{ color: "blue" }} />
 			</p>
-
-			<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
 			<p className="lead">
+				Welcome to SpeedyRX, The worlds largest distrubutor of prescription medication. Where we assist our
+				patrons with thier medication needs, while having a relationship with physcains to keep update medical
+				records
 				<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwebneel.com%2Fpharmacy-logo&psig=AOvVaw3K6vgIubfnmoHYz4HsKsyx&ust=1618080646349000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJjSiKPq8e8CFQAAAAAdAAAAABAV" />
 			</p>
 		</div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,5 +7,14 @@ export const Home = () => (
 	<div className="text-center mt-5">
 		<Jumbo />
 		<br />
+		<div className="row">
+			<button type="button" className="btn btn-primary col-6">
+				Customer Login
+			</button>
+			<br />
+			<button type="button" className="btn btn-warning col-6">
+				Admin Log in
+			</button>
+		</div>
 	</div>
 );
