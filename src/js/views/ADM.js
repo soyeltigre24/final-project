10540303a360b8965e8829a9973b51a96dd7d754
soyeltigre24 <Jairo.Ariza001@mymdc.net>
2021-04-09 23:54:30 +0000

    added admin js file

---
+++ src/js/views/ADM.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+const ADM = () => {
+	return <div className="modal-dialog modal-dialog-centered">hello</div>;
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,19 +2,25 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import Jumbo from "../views/Jumbo";
+import { Link } from "react-router-dom";
+import ADM from "./ADM";
 
 export const Home = () => (
 	<div className="text-center mt-5">
 		<Jumbo />
 		<br />
 		<div className="row">
-			<button type="button" className="btn btn-primary col-6">
-				Customer Login
-			</button>
+			<Link>
+				<button type="button" className="btn btn-primary col-6">
+					Customer Login
+				</button>
+			</Link>
 			<br />
-			<button type="button" className="btn btn-warning col-6">
-				Admin Log in
-			</button>
+			<Link>
+				<button type="button" className="btn btn-warning col-6">
+					Admin Log in
+				</button>
+			</Link>
 		</div>
 	</div>
 );
