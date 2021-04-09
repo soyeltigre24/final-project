1a449d72bd2568c20db650ec3fd7bf18d1413b8f
soyeltigre24 <Jairo.Ariza001@mymdc.net>
2021-04-09 20:06:24 +0000

    home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,7 +2,6 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import Jumbo from "../views/Jumbo";
-import CLG from "./views/CLG";
 
 export const Home = () => (
 	<div className="text-center mt-5">
